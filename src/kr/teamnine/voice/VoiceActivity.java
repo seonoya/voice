81a1aae81441a28074eb4433cb1ba6062e96c985
senku <senku7@gmail.com>
2012-06-02 15:13:12 +0900

    delete

--- src/kr/teamnine/voice/VoiceActivity.java
+++ src/kr/teamnine/voice/VoiceActivity.java
@@ -4,7 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class VoiceActivity extends Activity {
-    /** Called when the activity is first created. */
+    /** Called the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
