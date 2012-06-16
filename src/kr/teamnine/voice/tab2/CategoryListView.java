13d4397ed7cd673a96b5fec800522b083dfa3df3
seonoya <seonoya@gmail.com>
2012-06-16 17:17:40 +0900

    카테고리 리스트 작업중!!!

--- src/kr/teamnine/voice/tab2/CategoryListView.java
+++ src/kr/teamnine/voice/tab2/CategoryListView.java
@@ -2,16 +2,34 @@ package kr.teamnine.voice.tab2;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import kr.teamnine.voice.R;
 
-public class CategoryListView extends Activity {
+public class CategoryListView extends Activity implements AdapterView.OnItemClickListener {
 	/** Called when the activity is first created. */
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+    private String[] cars = {"SM3", "SM5", "SM7", "SONATA", "AVANTE", "SOUL", "K5", "K7"};
 
-		TextView textView = new TextView(this);
-		textView.setText("ег2");
-		setContentView(textView);
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tab2);
+
+        
+        ListView list = (ListView) findViewById(R.id.list);
+        
+        list.setAdapter(new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, cars));
+
+        list.setOnItemClickListener(this);        
+    }
+
+    public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+
+    
+    }	
+
 }
